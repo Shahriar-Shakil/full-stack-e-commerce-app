b8432deccc3f3fa1ccd9a2e634643f16a864623a
shahriar-shakil <toshahriarshakil@gmail.com>
2024-02-11 23:19:37 +0600

    add clerk/nextjs

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Hello admin dashboard from root</p>
+      <Button className="rounded-full" variant="secondary">
+        Button
+      </Button>
+    </div>
+  );
+}
